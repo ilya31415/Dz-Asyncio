881848c3044ce6b8ecbcbe0466f3625a15ae9455
Ilya <frostilia@mail.ru>
2022-08-04 22:08:42 +0300

    1

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,8 +1,5 @@
 import sqlite3 as sq
 
-list_current_keys = ['birth_year', 'eye_color', 'gender', 'hair_color', 'height', 'homeworld', 'mass', 'name',
-                     'skin_color', 'films', 'species', 'starships', 'vehicles']
-
 
 def sql_start():
     global base, cur
@@ -32,9 +29,10 @@ def sql_start():
 
 async def load_one_people(people):
     data = (
-    people["id"], people["birth_year"], people["eye_color"], people["gender"], people["hair_color"], people["height"],
-    people["homeworld"],
-    people["mass"], people["name"], people["films"], people["species"], people["starships"], people["vehicles"],
-    people["skin_color"])
+        people["id"], people["birth_year"], people["eye_color"], people["gender"], people["hair_color"],
+        people["height"],
+        people["homeworld"],
+        people["mass"], people["name"], people["films"], people["species"], people["starships"], people["vehicles"],
+        people["skin_color"])
     cur.execute(f'''INSERT INTO people VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', data)
     base.commit()
